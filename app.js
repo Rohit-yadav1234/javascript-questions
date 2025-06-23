f04d1946ad62d0f6e0101d44967f29a58fa42e61
Rohit-singhal-crypto <rohityadavrohit8361@gmail.com>
2025-06-23 12:28:34 +0530

    practice

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
-let n = 6; // total number of rows questions one
-for (let i = 1; i <= n; i++) {
-  let space = " ".repeat(n - i);
-  let stars = "*".repeat(2 * i - 1);
-  console.log(space + stars);
-}
-//     *
-//    ***
-//   *****
-//  *******
-// *********
+// let a = 1
+// console.log(a++) // 1
 
-// isPalindrome
-function isPalindrome(word) {
-  return word === word.split("").reverse().join("");
-}
-console.log(isPalindrome("naman"));
+// let b = 1
+// console.log(++b) // 2
 
-// true
+// let a = 1
+// console.log(a++ + ++a) // 4
+// console.log(a++ - ++a) // -2
+// console.log(a++ + ++a + a)  // 7
 
-// nth sum
-let num = 10;
-let sum = 0;
-for (let i = 2; i <= num; i = i + 2) {
-  sum = sum + i;
-}
-console.log(sum);
+// console.log([] == [])  //false
+// console.log(+ "5") // 5
+// console.log(typeof NaN)  // number
+// console.log("0" == "0") // true
+// console.log("0" === 0)  //false es m value b same honi chaiya or data type b
+// console.log(null) // null
 
-// output:30
 
-let age = prompt("Please enter your age:");
-if (age < 18) {
-  alert("Sorry, you must be at least 18 years old!");
-} else {
-  alert("Welcome! You are eligible.");
-}
+// let n = 6;
+// for (let i = 7; i <= n; i++) {
+//   let space = " ".repeat(n - i);
+//   let stars = "*".repeat(2 * i - 1);
+//   console.log(space + stars);
+// }
+// //     *
+// //    ***
+// //   *****
+// //  *******
+// // *********
+
+// // isPalindrome
+// function isPalindrome(word) {
+//   return word === word.split("").reverse().join("");
+// }
+// console.log(isPalindrome("naman"));
+// // true
+
+// // nth sum
+// let num = 10;
+// let sum = 0;
+// for (let i = 2; i <= num; i = i + 2) {
+//   sum = sum + i;
+// }
+// console.log(sum);
+// // output:30
+
+// let age = prompt("Please enter your age:");
+// if (age < 18) {
+//   alert("Sorry, you must be at least 18 years old!");
+// } else {
+//   alert("Welcome! You are eligible.");
+// }
 // 17  Sorry, you must be at least 18 years old!
 // 20  Welcome! You are eligible.
+
+// callback function
+// function add(a,b){
+//   return a+b;
+// }
+// function divide(a,b){
+//   return a/b;
+// }
+// function main(a,b,operation){
+//   return operation(a,b)
+// }
+// console.log(main(2,6,add))
+// console.log(main(2,2,divide))
